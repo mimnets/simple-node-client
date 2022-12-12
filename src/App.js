c53088beeac747f310ffdaab55e7d8a76db1356d
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-12 19:16:04 +0600

    show users

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { useEffect, useState } from 'react';
 function App() {
   const [users, setUsers] = useState([])
 
+
+  //Get data form node js server
   useEffect(()=>{
     fetch('http://localhost:5000/users')
     .then(res => res.json())
     .then(data => setUsers(data))
   },[])
 
+  //Post data to the node js server
 const handleAddUser = (event)=>{
   event.preventDefault();
   const form = event.target;
@@ -17,6 +20,20 @@ const handleAddUser = (event)=>{
   const email = form.email.value;
   const user = {name, email};
   console.log(user);
+  fetch('http://localhost:5000/users',{
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(user),
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    const newUser = [...users, data];
+    setUsers(newUser);
+  })
+  .catch(err => console.error(err));
   form.reset();
 }
 
@@ -24,14 +41,14 @@ const handleAddUser = (event)=>{
   return (
     <div className="App">
       <form onSubmit={handleAddUser}>
-        <input type="text" name="name"/>
+        <input type="text" name="name" placeholder='Name'/>
         <br/>
-        <input type="email" name="email"/>
+        <input type="email" name="email" placeholder='Email'/>
         <br/>
         <button>Add User</button>
       </form>
       {
-        users.map(user => <p key={user.id}>{user.name}</p>)
+        users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
       }
     </div>
   );
