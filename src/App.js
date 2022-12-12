6a54feb74447b3f877555ae9d81f96bb990a5fd1
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-12 15:34:35 +0600

    First simple node client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+
+const handleAddUser = (event)=>{
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const user = {name, email};
+  console.log(user);
+  form.reset();
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name"/>
+        <br/>
+        <input type="email" name="email"/>
+        <br/>
+        <button>Add User</button>
+      </form>
+      {
+        users.map(user => <p key={user.id}>{user.name}</p>)
+      }
     </div>
   );
 }
